492cbc023013e5901122ff910db36eb0eb2876f7
Abhhhinav <107941084+Abhhhinav@users.noreply.github.com>
2023-07-14 12:54:30 +0530

    Add files via upload

---
+++ TopoSortBFS.java
@@ -0,0 +1,69 @@
+package Graphs;
+import java.util.*;
+public class TopoSortBFS {
+    static class Edge {
+        int src;
+        int des;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.des = d;
+        }
+    }
+    static void createGraph(ArrayList<Edge> graph[]) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 1));
+
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+    }
+    
+    public static void CalcInd(ArrayList<Edge> graph[], int indeg[]) {
+        for (int i = 0; i < graph.length; i++) {
+            int v = i;
+            for (int j = 0; j < graph[v].size(); j++) {
+                Edge e = graph[v].get(j);
+                indeg[e.des]++;
+            }
+        }
+    }
+    public static void TopSort(ArrayList<Edge> graph[]) {
+        int indeg[] = new int[graph.length];
+        CalcInd(graph, indeg);
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0) {
+                q.add(i);
+            }
+        }
+        // bfs
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            System.out.println(curr + "");
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                indeg[e.des]--;
+                if (indeg[e.des] == 0) {
+                    q.add(e.des);
+                }
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int V = 6;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        TopSort(graph);
+
+
+    }
+}
